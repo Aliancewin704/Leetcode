1d5bf114e652ed5969a792920011302827cec2d0
AlexRimC9 <alexrimC9@gmail.com>
2023-08-23 10:41:40 +0300

    Commit 14 Neither Minimum nor Maximum 23.08.2023

---
+++ org/example/Fundamental/NeitherMinimumNorMaximum/Main.java
@@ -0,0 +1,8 @@
+package org.example.Fundamental.NeitherMinimumNorMaximum;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(NeitherMinimumNorMaximum.findNonMinOrMax(new int[]{1, 2,3,4}));
+    }
+}
---
+++ org/example/Fundamental/NeitherMinimumNorMaximum/NeitherMinimumNorMaximum.java
@@ -0,0 +1,25 @@
+package org.example.Fundamental.NeitherMinimumNorMaximum;
+
+public class NeitherMinimumNorMaximum {
+
+    public static int findNonMinOrMax(int[] nums) {
+        if (nums.length < 3) return -1;
+        int max = nums[0];
+        int min = nums[0];
+        for (int i = 1; i <= nums.length - 1; i++) {
+            if (nums[i] < min) {
+                if (min < max) return min;
+                min = nums[i];
+                continue;
+            }
+            if (nums[i] > max) {
+                if (min < max) return max;
+                max = nums[i];
+                continue;
+            }
+            if (nums[i] == min || nums[i] == max) continue;
+            return nums[i];
+        }
+        return -1;
+    }
+}
