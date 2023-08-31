d9181a58d0ca02d6cc45624b3be0be13e8661cb9
AlexRimC9 <alexrimC9@gmail.com>
2023-08-31 10:39:57 +0300

    Commit 25 Remove Element 31.08.2023

---
+++ org/example/Fundamental/Remove_Element/Main.java
@@ -0,0 +1,6 @@
+package org.example.Fundamental.Remove_Element;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
---
+++ org/example/Fundamental/Remove_Element/RemoveElement.java
@@ -0,0 +1,15 @@
+package org.example.Fundamental.Remove_Element;
+
+public class RemoveElement {
+
+    public static int removeElement(int[] nums, int val) {
+        int k = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != val){
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
