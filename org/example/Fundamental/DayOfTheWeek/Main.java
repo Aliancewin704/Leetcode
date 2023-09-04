85e5eee892bb2fcaa7d4f631b6e6d475d3bcab2d
AlexRimC9 <alexrimC9@gmail.com>
2023-09-04 10:08:21 +0300

    Commit 29 Day Of The Week 04.09.2023

---
+++ org/example/Fundamental/DayOfTheWeek/Main.java
@@ -0,0 +1,7 @@
+package org.example.Fundamental.DayOfTheWeek;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(DayOfTheWeek.dayOfTheWeek(18,7,1999));
+    }
+}
