1347e50e029776b236f049540f459ee6e2b1f087
AlexRimC9 <alexrimC9@gmail.com>
2023-09-19 12:42:05 +0300

    Commit 38 Partition Labels 19.09.2023

---
+++ org/example/Fundamental/Partition_Labels/Main.java
@@ -0,0 +1,7 @@
+package org.example.Fundamental.Partition_Labels;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(PartitionLabels.partitionLabels("ababcbacadefegdehijhklij"));
+    }
+}
