7c05b724e71eeb479005a5a99c52114191ca04be
AlexRimC9 <alexrimC9@gmail.com>
2023-09-19 10:58:56 +0300

    Commit 37 Search in Rotated Sorted Array 19.09.2023

---
+++ org/example/Fundamental/Search_In_Rotated_Sorted_Array/Main.java
@@ -0,0 +1,7 @@
+package org.example.Fundamental.Search_In_Rotated_Sorted_Array;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(SearchInRotatedSortedArray.search(new int[]{1},2));
+    }
+}
---
+++ org/example/Fundamental/Search_In_Rotated_Sorted_Array/SearchInRotatedSortedArray.java
@@ -0,0 +1,32 @@
+package org.example.Fundamental.Search_In_Rotated_Sorted_Array;
+
+public class SearchInRotatedSortedArray {
+    public static int search(int[] nums, int target) {
+        if (nums.length == 1) {
+            return nums[0] == target ? 0 : -1;
+        }
+        if (nums[nums.length - 1] < target) {
+            if (nums[0] > target) {
+                return -1;
+            } else {
+                for (int i = 0; i < nums.length; i++) {
+                    if (nums[i] == target) return i;
+                    if (nums[i] > target) return -1;
+                }
+            }
+        } else {
+            if (nums[0] > target) {
+                for (int i = nums.length - 1; i > 0; i--) {
+                    if (nums[i] == target) return i;
+                    if (nums[i] < target) return -1;
+                }
+            } else {
+                for (int i = 0; i < nums.length; i++) {
+                    if (nums[i] == target) return i;
+                    if (nums[i] > target) return -1;
+                }
+            }
+        }
+        return -1;
+    }
+}
