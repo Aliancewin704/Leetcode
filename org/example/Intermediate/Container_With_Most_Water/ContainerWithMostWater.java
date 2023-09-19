19776b3a31c13f2ff4bb86c337b487ceeab7e2e0
AlexRimC9 <alexrimC9@gmail.com>
2023-09-19 13:08:29 +0300

    Commit 39 Container With Most Water 19.09.2023

---
+++ org/example/Intermediate/Container_With_Most_Water/ContainerWithMostWater.java
@@ -0,0 +1,23 @@
+package org.example.Intermediate.Container_With_Most_Water;
+
+public class ContainerWithMostWater {
+    public static int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int currentArea = Math.min(height[left], height[right]) * (right - left);
+            maxArea = Math.max(maxArea, currentArea);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+
+}
---
+++ org/example/Intermediate/Container_With_Most_Water/Main.java
@@ -0,0 +1,7 @@
+package org.example.Intermediate.Container_With_Most_Water;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(ContainerWithMostWater.maxArea(new int[]{1,8,6,2,5,4,8,3,7}));
+    }
+}
