a68ef69c842576a46d71e338eddc796b0c98ab33
ABBU <aliancewin704@gmail.com>
2026-01-29 15:51:14 +0300

    Revert "Commit 47"
    This reverts commit 7d78bc1e8067974c53646c7be72aee73dddaf3e3.

--- org/example/Intermediate/Minimum_Distance_Between_Three_Equal_Elements_II/Main.java
+++
@@ -1,8 +0,0 @@
-package org.example.Intermediate.Minimum_Distance_Between_Three_Equal_Elements_II;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println(MinimumDistanceBetweenThreeEqualElementsII.minimumDistance(new int[]{26,12,24,11,1,16,16,7,25,3,21,6,19,8,30,26,14,21,23,5,28,16,1,21,13,31,6,7,29,20,17}));
-    }
-}
